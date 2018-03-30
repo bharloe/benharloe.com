19a3f0dcda0614485b8a423091f66cbabe5876fa
Ben Harloe <ben.harloe@gmail.com>
2018-03-30 16:36:55 -0700

    website 1.0 complete

--- client/assets/js/scripts.js
+++ client/assets/js/scripts.js
@@ -10,14 +10,14 @@ $(document).ready(function () {
             message: $("#message").val()
         }
         
-
         $.ajax({
             method: "POST",
             url: "https://5ee76gb3pb.execute-api.us-east-1.amazonaws.com/dev/",
-            data: data,
-            success: function (response) {
-                console.log("test");
-            }
+            data: data
         });
+
+        $('#contactHeader').text('Thanks for the message! I will be in touch via email soon.')
+        $('#contactForm').fadeTo( "slow" , 0.3, function() {});
+        $('#contactFormSection').hide('slow', function() {});
     });
 });
